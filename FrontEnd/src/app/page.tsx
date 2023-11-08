3689a2390f3f2a774422946126ffb724da10b828
Martin Kopecký <kopecky@thekrew.app>
2023-11-08 08:45:29 +0100

    Auth update - API removed
    stagUserInfo ma informace o uzivately a ne o jeho roli tak je to podle Stag dokumentace

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,24 +1,4 @@
-'use client';
-import { useEffect } from "react";
-import { Set } from "./actions";
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    if (!searchParams.has("osCislo")) {
-      // Redirect to "/login" if "osCislo" query parameter is not present
-      // window.location.href = "/login";
-    } else {
-      const params: any = {
-        osCislo: searchParams.get("osCislo"),
-      };
-      console.log(params);
-      Set("osCislo", params.osCislo);
-    }
-    redirect('/');
-  }, []);
-
   return (
     <main>
       <div className="container-row">
