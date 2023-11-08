20cdc82fca738e83b3a515f1b45441a1b7265b86
DanielRiha8906 <daniel.riha8906@seznam.cz>
2023-11-08 17:34:09 +0100

    Table student

--- FrontEnd/src/app/student/profil/page.tsx
+++ FrontEnd/src/app/student/profil/page.tsx
@@ -1,18 +1,14 @@
-import { Categorie } from "@/data/categorie";
-import Category from "@/modules/category.module";
 import { User } from "../clientSide";
-
+import Terminy from "@/modules/terminy";
 export default function ProfilePage() {
     return (
+        <>
             <div className="profile flex self-start">
                <div className="user">
                    <User/>
                </div>
-               <div className="cats">
-                    {Categorie.map((cat) => (
-                        <Category key={cat.title} {...cat} />
-                    ))}
-               </div>
+               <Terminy/>
             </div>
+        </>
     )
 }
