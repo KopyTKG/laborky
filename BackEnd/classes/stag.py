ef44d8a0510d0bcdb8e15188e8e163fa46d002a7
_alex <93431802+Bumross@users.noreply.github.com>
2024-09-27 21:02:10 +0200

    structure of API

--- BackEnd/classes/stag.py
+++ BackEnd/classes/stag.py
@@ -1,6 +1,6 @@
 import requests
 
-def Get(ticket, url, params):
+def get(ticket, url, params):
     headers = {
         "accept": "application/json",
         "Content-Type": "application/json",
@@ -13,8 +13,8 @@ def Get(ticket, url, params):
     return response.json()
 
 
-def GetStagUser(Ticket):
-    url = "https://stag-demo.zcu.cz/ws/services/rest2/help/getStagUserListForLoginTicketV2?ticket=" + Ticket 
+def get_stag_user_info(ticket):
+    url = "https://stag-demo.zcu.cz/ws/services/rest2/help/getStagUserListForLoginTicketV2?ticket=" + ticket 
     headers = {
         "accept": "application/json",
         "Content-Type": "application/json",
@@ -30,53 +30,15 @@ def GetStagUser(Ticket):
 
 
 
-
-def GetPredmetyByStudent(Ticket):
-    url = "https://stag-demo.zcu.cz/ws/services/rest2/predmety/getPredmetyByStudent"
-    params = {
-        "osCislo": "F22153",
-        "semestr": "ZS"
-    }
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "Connection": "keep-alive", 
-        "Accept-Origin": "https://stag-demo.zcu.cz",
-    }
-    response = requests.get(url,headers=headers, params=params, cookies={'WSCOOKIE': Ticket})
-    if not response.ok:
-        raise Exception(response.text)
-    return response.json()
-
-
-def GetStudentPredmetyAbsolvoval(Ticket, function):
-    url = "https://stag-demo.zcu.cz/ws/services/rest2/student/getStudentPredmetyAbsolvoval"
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "Connection": "keep-alive", 
-        "Accept-Origin": "https://stag-demo.zcu.cz",
-    }
-    response = requests.get(url,headers=headers, cookies={'WSCOOKIE': Ticket})
-    if not response.ok:
-        raise Exception(response.text)
-    splneno = []
-    aktivni_predmety = []
-    predmety = response.json()
-    for predmet in predmety["predmetAbsolvoval"]:
-        if predmet["zkratka"] in function:
-            aktivni_predmety.append(predmet["zkratka"]) if predmet["absolvoval"] == "N" else splneno.append(predmet["zkratka"])
-    predmety = [item for item in aktivni_predmety if item not in splneno]
-    return aktivni_predmety, splneno, predmety
-
-
-def GetUcitelPredmety(Ticket):
+# Asi není potřeba - pouze pro to, kdyby mohl učitel vypisovat pouze na svoje předměty
+def get_ucitel_predmety(ticket, ucitIdno):
     """
-     Vrati predmety, ktery ucitel vyucuje 
+     Vrati predmety, ktere ucitel vyucuje 
     """
+
     url = "https://stag-demo.zcu.cz/ws/services/rest2/predmety/getPredmetyByUcitel"
     params = {
-        "ucitIdno": "49517",
+        "ucitIdno": ucitIdno,
     }
     headers = {
         "accept": "application/json",
@@ -84,7 +46,7 @@ def GetUcitelPredmety(Ticket):
         "Connection": "keep-alive", 
         "Accept-Origin": "https://stag-demo.zcu.cz",
     }
-    response = requests.get(url,headers=headers, params=params, cookies={'WSCOOKIE': Ticket})
+    response = requests.get(url,headers=headers, params=params, cookies={'WSCOOKIE': ticket})
     if not response.ok:
         raise Exception(response.text)
     return response.json()
--- BackEnd/server.py
+++ BackEnd/server.py
@@ -3,19 +3,127 @@ from fastapi import FastAPI
 from classes.stag import *
 from classes.db import *
 from classes.vyucujici import *
+from classes.student import *
 from jose import jwt
 import dotenv, os, requests, json
-import uvicorn
 
 
 dotenv.load_dotenv()
 
 app = FastAPI(debug=True)
 
+
+@app.get("/setup")
+async def setup(): # prijima parametr ticket
+    """ Kontrola přihlášeného uživatele s databází po loginu do systému """
+    ticket = os.getenv('TICKET') # prozatimni reseni
+
+    # provede:
+        # role uzivatele =
+        # userId uzivatele =
+
+        # check s databazi
+        # vrati OK 200?
+
+    return "login do databaze"
+
+
+### Student API
+## /STUDENT HOME
+@app.get("/student/{osobni_cislo}")
+async def get_student_home(): #prijima parametr ticket
+    """ Vrácení všech vypsaných laborek podle toho, na co se student může zapsat """
+    ticket = os.getenv('TICKET') # prozatimni reseni
+
+    # provede:
+        # laborky jakeho seminare ma student na vyber =
+        # jake jsou laborky k dispozici na dany predmet (DB)=
+        
+        # vraci json vsech laborek, ktere jsou k dispozici
+        # student na nich neni zapsan!!
+
+    return "student home"
+
+
+@app.post("/student/{osobni_cislo}/{id_lab}")
+async def add_student_to_lab(): # prijima argument id labu, na ktery se student registruje
+    """ Zaregistruje studenta do labu, který je k dispozici na jeho hlavní straně """
+
+    # provede:
+        # DB relace mezi studentem a labem
+        # prida do laborek +1 na obsazenost
+
+        # vraci ok 200?
+
+
+## /STUDENT MOJE
+@app.get("/student/{osobni_cislo}/moje")
+async def get_student_moje(): #prijima parametr ticket
+    """ Vrátí cvičení, na kterých je student aktuálně zapsán"""
+    ticket = os.getenv('TICKET') # prozatimni reseni
+
+    # provede:
+        # DB podle jeho osobniho cisla, kde je student zapsan =
+
+        # vraci seznam cviceni, ktere ma student zapsany
+
+    return "student zapsane terminy"
+
+
+@app.post("/student/{osobni_cislo}/moje/{id_lab}")
+async def remove_student_from_lab(id_lab):
+    """ Odhlášení studenta ze cvičení podle id cvičení """
+
+    # provede:
+        # zruší relaci mezi studentem a cvičením
+        # odečte 1 od obsazenosti cvičení
+
+        # vraci ok 200?
+
+
+## /STUDENT PROFIL
+@app.get("/student/{osobni_cislo}/profil")
+async def get_student_profil(): #prijima parametr ticket
+    """ Vraci zaznam o vsech typech cviceni na dany seznam predmetu, zda je student splnil ci nikoli"""
+    ticket = os.getenv("TICKET") # prozatimni reseni
+
+    # provede:
+        # DB podle osobniho cisla, vrati zda student ma v minulosti splneno jaky typ cviceni
+        # to, co nema oznaceno jako splnil a nebo je zapsan a hodina jeste neprobehla, ma jako nesplnil
+
+        # vraci predmet - seminare a u kazdeho jestli splnil nebo ne
+
+    return "student profil"
+
+
+### Ucitel API 
+## /UCITEL HOME
+
+
+
+
+
+## /UCITEL PREDMETY
+
+
+
+
+## /UCITEL STUDENTI
+@app.get("/ucitel/studenti")
+async def get_vypis_studentu(): #prijima parametr ticket
+    """ Vrácení všech studentů, kteří se zapsali na daný seminář"""
+    ticket = os.getenv('TICKET') # prozatimni reseni
+
+    return "x"
+
+
+
+
+
 @app.get("/")
 async def root():
     ticket = os.getenv('TICKET')
-    function = Tracked_Predmety()
+    predmety = predmety_pro_cviceni()
     user = GetUcitelPredmety(ticket) 
     return user
 
@@ -31,7 +139,9 @@ def GetInfo(ticket):
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv('PORT')))
 
-    
+
+
+
 """
 url = "https://stag-demo.zcu.cz/ws/services/rest2/rozvrhy/getRozvrhByMistnost"
 params = {
