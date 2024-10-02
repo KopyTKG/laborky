97c36259472bc3b65a7e0d10005a49e568ab0b18
Martin Kopecký <kopecky@thekrew.app>
2024-10-02 12:40:12 +0200

    connection login to fastapi

--- FrontEnd/src/lib/stag.ts
+++ FrontEnd/src/lib/stag.ts
@@ -1,3 +1,10 @@
+export const fastHeaders = new Headers({
+ accept: 'application/json',
+ 'Content-Type': 'application/json',
+ Connection: 'keep-alive',
+ Origin: process.env.NEXT_PUBLIC_API_URL || '',
+})
+
 export async function getStagUser(ticket: string): Promise<any> {
  const url = `https://stag-demo.zcu.cz/ws/services/rest2/help/getStagUserListForLoginTicketV2?ticket=${ticket}`
  const headers = {
