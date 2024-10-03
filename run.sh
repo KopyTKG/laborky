04ede566b6b15222f3b544ce18dfdcb00fbe3522
Alex Schönfelder <93431802+Bumross@users.noreply.github.com>
2024-10-03 15:12:35 +0200

    patch run.sh

--- run.sh
+++ run.sh
@@ -27,5 +27,5 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 else
     echo "Windows"
     start powershell -Command 'cd FrontEnd; npm run dev'
-    start powershell -Command 'cd BackEnd; python -m venv venv; venv/Scripts/activate; uvicorn server:app --reload'
+    start powershell -Command 'cd BackEnd; python -m venv venv; venv/Scripts/activate; python server.py'
 fi
