51881e90a6ee8c86600fb98fbf2deab76a47646e
DanielRiha8906 <daniel.riha8906@seznam.cz>
2024-10-08 21:24:53 +0200

    not

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -292,7 +292,7 @@ async def ucitel_vytvor_termin(ticket: str, termin: tTermin):
     if termin.kod_predmetu is None:
         return not_found
 
-    if termin.vyucuje_prijmeni is not None:
+    if not termin.vyucuje_prijmeni:
         vyucuje_id = get_vyucujiciho_by_predmet(session, termin.kod_predmetu)[0] # type: ignore
 # TODO: vymyslet, jak se bude vkládat id vyučujícího bez toho, aniž by admin, který není vyučující termínu, ale vypisující, mohl vypsat termín na 1 vyučujícího
 # navrh: random()
