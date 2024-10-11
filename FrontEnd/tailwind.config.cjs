8c3d1b6bcc906778ddc526f70dacb85101ce00ad
Martin Kopecký <kopecky@thekrew.app>
2024-10-11 20:02:59 +0200

    adding FE to docker

--- FrontEnd/tailwind.config.cjs
+++ FrontEnd/tailwind.config.cjs
@@ -1,8 +1,6 @@
 import { nextui } from '@nextui-org/react'
-/** @type {import('tailwindcss').Config} */
 module.exports = {
  content: [
-  // Or if using `src` directory:
   './src/**/*.{js,ts,jsx,tsx,mdx}',
   './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
  ],
