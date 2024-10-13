4925741296be00bc73289741040fc05e076c682f
Alex Schönfelder <93431802+Bumross@users.noreply.github.com>
2024-10-13 14:59:41 +0200

    .

--- BackEnd/endpoints/ucitel/student/student_zapsat.py
+++ BackEnd/endpoints/ucitel/student/student_zapsat.py
@@ -9,8 +9,8 @@ router = APIRouter()
 async def post_ucitel_zapsat_studenta(ticket: str, id_stud: str, id_terminu: str): #ticket: str | None = None, id_stud: str | None = None
     """ Ručně přihlásí studenta do vypsaného termínu cvičení
 
-    Args: id_studenta je jeho Fčíslo s F na začátku! [F*****] (nutno připsat dovnitř vstupu)"""
-    # ticket = os.getenv("TICKET")
+    Args: id_studenta je jeho osobní číslo"""
+
     info = kontrola_ticketu(ticket, vyucujici=True)
     if info == unauthorized or info == internal_server_error:
         return info
