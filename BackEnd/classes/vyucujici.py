e8d37123104e6c0d173961f90d557a6f3a3498ae
Alex Schönfelder <93431802+Bumross@users.noreply.github.com>
2024-10-16 21:27:51 +0200

    změna vytvoření termínu - přidání vyučujícího
    když ten, kdo vypisuje termín nebude termín vyučovat, tak je možnost vyhledat vyučujícího podle jména a příjmení
    
    - ze stagu to najde jeho id a vloží ho jako vyučujícího do databáze
    
    - pros: učitel nemusí být registrovaný a i tak mu to přidá termín
    - cons: když jsou nějací jmenovci, přiřadí to tomu prvnímu, kterého najde v seznamu :-)

--- BackEnd/classes/vyucujici.py
+++ BackEnd/classes/vyucujici.py
@@ -167,3 +167,24 @@ def get_vyucujici_predmety(ticket, predmety_db):
     return predmety_ucitele
     
 
+def get_id_ucitele_by_jmeno_prijmeni(ticket, jmeno, prijmeni):
+    """ Získá F čísla všech studentů, kteří jsou zapsáni na předmětu """
+    url = os.getenv("STAG_URL") + "ws/services/rest2/ucitel/najdiUcitelePodleJmena"
+    params = {
+        "prijmeni": prijmeni,
+        "jmeno": jmeno,
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Connection": "keep-alive",
+        "Accept-Origin": os.getenv("STAG_URL"),
+    }
+    try:
+        response = requests.get(url, params=params, headers=headers, cookies={'WSCOOKIE': ticket})
+        response = response.json()["ucitel"][0]
+        if len(response) == 0:
+            return []
+        return str(response["ucitIdno"])
+    except:
+        return not_found
--- BackEnd/routes/ucitel/termin/termin_vytvor.py
+++ BackEnd/routes/ucitel/termin/termin_vytvor.py
@@ -15,34 +15,27 @@ async def ucitel_vytvor_termin(ticket: str, termin: tTermin):
     if termin.kod_predmetu is None:
         return not_found
 
-    if not termin.vyucuje_prijmeni:
-        vyucuje_id = get_vyucujiciho_by_predmet(session, termin.kod_predmetu)
-        if vyucuje_id == internal_server_error or not vyucuje_id:
-            return internal_server_error
-        vyucuje_id = vyucuje_id[0] # type: ignore
+    if termin.vyucuje_prijmeni is not None:
+        vyucuje_id = get_id_ucitele_by_jmeno_prijmeni(ticket, str(termin.vyucuje_jmeno), str(termin.vyucuje_prijmeni))
+        if vyucuje_id == not_found or vyucuje_id == []:
+            return not_found
+        vyucuje_id = encode_id(vyucuje_id) # type: ignore
+    else:
+        vyucuje_id = vypsal_id
 
     if termin.datum_start > termin.datum_konec:
         promenna_na_prohazeni = termin.datum_start
         termin.datum_start = termin.datum_konec
         termin.datum_konec = promenna_na_prohazeni
-# TODO: vymyslet, jak se bude vkládat id vyučujícího bez toho, aniž by admin, který není vyučující termínu, ale vypisující, mohl vypsat termín na 1 vyučujícího
-# navrh: random()
-
-
-    else:
-        vyucuje_id = vypsal_id
 
     message = vypsat_termin(session, termin.ucebna, termin.datum_start, termin.datum_konec, termin.max_kapacita, vypsal_id, vyucuje_id, termin.kod_predmetu, termin.jmeno, termin.cislo_cviceni, termin.popis) # type: ignore
-    return message
-#TODO: upozornění emailů
-    # if message is not ok:
-    #     return message
-    # elif upozornit:
-    #     katedra = get_katedra_by_predmet(session, zkratka_predmetu)
-    #     kod_predmetu = katedra + "/" +zkratka_predmetu # type: ignore
-    #     list_emailu = get_list_emailu_by_predmet(session, kod_predmetu, cislo_cviceni)
-    #     if list_emailu == []:
-    #         return not_found
-    #     return list_emailu
-    # else:
-    #     return ok
+    if message is not ok:
+        return message
+
+#TODO: upozornění na termíny     
+    #if termin.upozornit:
+    #    list_emailu = get_list_emailu_pro_cviceni(session, termin.kod_predmetu, termin.cislo_cviceni)
+    #    return list_emailu
+    
+    else:
+        return ok
