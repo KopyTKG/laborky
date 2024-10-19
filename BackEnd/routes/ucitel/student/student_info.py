2c4c8b5ac45c61b68ad4760cccbf522bd452a9cb
DanielRiha8906 <129500714+DanielRiha8906@users.noreply.github.com>
2024-10-19 19:19:47 +0200

    Dev nevyzkouseno (#86)
    Co-authored-by: _alex <93431802+Bumross@users.noreply.github.com>

--- BackEnd/routes/ucitel/student/student_info.py
+++ BackEnd/routes/ucitel/student/student_info.py
@@ -17,6 +17,17 @@ async def get_ucitel_studenta(ticket: str, id_stud: str):
     if info == unauthorized or info == internal_server_error:
         return info
 
+    userid, role = info[0], info[1]
+
+    predmety_ucitele = get_predmety_by_vyucujici(session, userid)
+    if predmety_ucitele == internal_server_error:
+        return internal_server_error
+    
+    kody_predmetu_ucitele = []
+    for predmet in predmety_ucitele:
+        kody_predmetu_ucitele.append(predmet.kod_predmetu)
+
+
     predmety_studenta = get_student_predmety(ticket, id_stud, get_vsechny_predmety_obj(session))
     if predmety_studenta is not_found:
         return not_found
@@ -33,7 +44,6 @@ async def get_ucitel_studenta(ticket: str, id_stud: str):
     student_db = get_student_by_id(session, id_stud)
     if student_db == None or student_db == not_found:
         message = vytvor_student(session, id_stud)
-        print("student nebyl v DB")
         if message != ok:
             return message
 
@@ -44,7 +54,7 @@ async def get_ucitel_studenta(ticket: str, id_stud: str):
     vyhodnoceni = {}
 
     for predmet in predmety_studenta:
-        if predmet in list(vyhodnoceni_vsech_predmetu.keys()):
+        if predmet in list(vyhodnoceni_vsech_predmetu.keys()) and predmet in kody_predmetu_ucitele:
             vyhodnoceni[predmet] = vyhodnoceni_vsech_predmetu[predmet]
 
     student_full = {"info": student_info, "profil": vyhodnoceni}
