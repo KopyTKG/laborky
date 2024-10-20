41965a3f22ce447c30d698be0981e380cfa1e15e
Martin Kopecký <kopecky@thekrew.app>
2024-10-20 10:19:08 +0200

    Misconfiguration of /admin/* routes

--- BackEnd/routes/admin/__init__.py
+++ BackEnd/routes/admin/__init__.py
@@ -1,9 +1,14 @@
 from .nadchazejici import router as admin_nadchazejici
 from .board import router as admin_board
-from .pridat_predmet import router as admin_pridat_predmet
+from .predmet.predmet_vytvor import router as admin_predmet_vytvorit
+from .predmet.predmet_smazat import router as admin_predmet_smazat
+from .predmet.predmet_zmena import router as admin_predmet_zmena
+
 
 admin_routers = [
     admin_nadchazejici,
     admin_board,
-    admin_pridat_predmet
+    admin_predmet_vytvorit,
+    admin_predmet_smazat,
+    admin_predmet_zmena
 ]
