24be34277badd8e534063b5957cd957622353cf3
Midiros <adamlegner99@gmail.com>
2024-10-21 18:59:05 +0200

    patch parsovani kodu predmetu

--- BackEnd/routes/admin/predmet/predmet_zmena.py
+++ BackEnd/routes/admin/predmet/predmet_zmena.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from classes.server_utils import *
 from datetime import datetime, timedelta
 from typing import Optional
+from urllib.parse import unquote
 
 router = APIRouter()
 
@@ -13,6 +14,7 @@ async def update_predmet(ticket: str, kod_predmetu:str, predmet: tPredmet):
     if info == unauthorized or info == internal_server_error:
         return info
 
-    vystup = upravit_predmet(session, kod_predmetu, newZkratkaPredmetu=predmet.zkratka_predmetu, newKatedra=predmet.katedra, newPocetCviceni=predmet.pocet_cviceni)
+    kod_predmetu = unquote(kod_predmetu)
 
+    vystup = upravit_predmet(session, kod_predmetu, newZkratkaPredmetu=predmet.zkratka_predmetu, newKatedra=predmet.katedra, newPocetCviceni=predmet.pocet_cviceni)
     return vystup
